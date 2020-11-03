b32b8f5d7ee8a19d9077f93a81130c6d75c3a21c
GokoshiJr <julioagonzalez18@gmail.com>
2020-11-03 18:08:49 -0400

    Struct y Receivers

--- src/calculadora.go
+++ src/calculadora.go
@@ -9,34 +9,49 @@ import (
 )
 
 func main() {
-	// creamos un objeto Scanner
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("\nIngrese una operacion: ")
-	scanner.Scan()
-	operacion := scanner.Text()
+	entrada := leerEntrada(false)
+	operador := leerEntrada(true)
+	// instanciar el struct
+	c := calc{}
+	fmt.Println("Resultado =", c.operate(entrada, operador))
+}
 
-	operador := "-"
-	numeros := strings.Split(operacion, operador)
-	num1, err1 := strconv.Atoi(numeros[0])
-	num2, err2 := strconv.Atoi(numeros[1])
-
-	// nil = null
-	if (err1 != nil ) { 
-		fmt.Println("Error1", err1) 
-	} else if (err2 != nil) {
-		fmt.Println("Error2", err2) 
+// Lee por consola
+func leerEntrada(operador bool) string {
+	scanner := bufio.NewScanner(os.Stdin) // creamos un objeto Scanner
+	if operador {
+		fmt.Print("Ingrese un operador (+,-,*,/): ")
 	} else {
-		switch operador {
-			case "+":
-				fmt.Println("Resultado:", num1 + num2)
-			case "-":
-				fmt.Println("Resultado:", num1 - num2)
-			case "*":
-				fmt.Println("Resultado:", num1 * num2)
-			case "/":
-				fmt.Println("Resultado:", num1 / num2)
-			default:
-				fmt.Println(operador, "No soportado")
-		}
+		fmt.Print("Ingrese la operacion completa (ej 1+1): ")
+	}
+	scanner.Scan() // activamos el scanner
+	return scanner.Text() // lee un string
+}
+
+// Transforma un string a un entero
+func parsear(entrada string) int {
+	num, _ := strconv.Atoi(entrada)
+	return num
+}
+
+// (calc) receirves function, le asignamos la funcion al struct calc
+func (calc) operate(entrada string, operador string) int {
+	entradaLimpia := strings.Split(entrada, operador)
+	num1 := parsear(entradaLimpia[0])
+	num2 := parsear(entradaLimpia[1])
+	switch operador {
+	case "+":
+		return num1 + num2
+	case "-":
+		return num1 - num2
+	case "*":
+		return num1 * num2
+	case "/":
+		return num1 / num2
+	default:
+		return 0
 	}
 }
+
+// Structs = Clases
+type calc struct {}
