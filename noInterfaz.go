8c5e5d690f52ebbbc81bdddba203f1703742f99c
GokoshiJr <julioagonzalez18@gmail.com>
2020-11-04 18:30:50 -0400

    Ejemplo sin Interfaz

---
+++ noInterfaz.go
@@ -0,0 +1,50 @@
+package main
+
+import("fmt")
+
+func main() {
+	perro1 := perro{}
+	moverPerro(perro1)
+
+	pez1 := pez{}
+	moverPez(pez1)
+
+	pajaro := pajaro{}
+	moverPajaro(pajaro)
+}
+
+type perro struct {
+
+}
+
+func (perro) caminar() string {
+	return "Soy un perro y camino"
+}
+
+func moverPerro(p perro) {
+	fmt.Println(p.caminar())
+}
+
+type pez struct {
+
+}
+
+func (pez) nadar() string {
+	return "Soy un pez y estoy nadando"
+}
+
+func moverPez(p pez) {
+	fmt.Println(p.nadar())
+}
+
+type pajaro struct {
+
+}
+
+func (pajaro) volar() string {
+	return "Soy un pajaro y estoy volando"
+}
+
+func moverPajaro(p pajaro) {
+	fmt.Println(p.volar())
+}
